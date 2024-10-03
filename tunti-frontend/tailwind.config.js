5eb3bd0524d4bab86f64ea83a50a65d7cf394ab0
Vainioleo <leo.vainio@solita.fi>
2024-10-03 11:48:02 +0300

    alusta shadcn ja tee esimerkkikomponentti

--- tunti-frontend/tailwind.config.js
+++ tunti-frontend/tailwind.config.js
@@ -1,21 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#4e8098',
-        secondary: '#ced3dc',
-        white: '#fcf7f8',
-        priceHigh: '#a31621',
-        priceMedium: '#fabc3c',
-        priceLow: '#71ba4f',
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#4e8098',
+  			secondary: '#ced3dc',
+  			white: '#fcf7f8',
+  			priceHigh: '#a31621',
+  			priceMedium: '#fabc3c',
+  			priceLow: '#71ba4f'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
