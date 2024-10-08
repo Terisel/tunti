80d43d504fa1d5bbf827ef5a647b7e0852020f95
Vainioleo <leo.vainio@solita.fi>
2024-10-08 13:50:27 +0300

    siirrä pricelist omaan component/domain kansioon

--- tunti-frontend/src/App.tsx
+++ tunti-frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react"
 import "./App.css"
 
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
+import { PriceList } from "@/components/domain/price-list"
 
 // Interfaces for Price Data
 interface PriceEntry {
@@ -31,51 +30,6 @@ const fetchPriceData = async (): Promise<PriceData> => {
   }
   const data: PriceData = await response.json() // Map directly to PriceData type
   return data
-}
-
-const getCurrentHour = (): string => {
-  const now = new Date()
-  return now.toISOString().substring(0, 13) + ":00:00.000Z" // Format as "YYYY-MM-DDTHH:00:00.000Z"
-}
-
-function PriceList({ prices }: PriceData) {
-  // Function to format the start date
-  const formatStartDate = (dateString: string): string => {
-    const date = new Date(dateString)
-    return date.toLocaleTimeString([], { weekday: "long", hour: "2-digit", minute: "2-digit" }) // Formats to day HH:mm
-  }
-  const currentHour = getCurrentHour()
-
-  return (
-    <ScrollArea className="rounded-md border">
-      <div className="p-4">
-        {prices.map((entry, index) => {
-          // Determine the color of the indicator based on price
-          let indicatorColor = "bg-green-500"
-          if (entry.price > 8 && entry.price < 15) {
-            indicatorColor = "bg-yellow-500"
-          } else if (entry.price > 15) {
-            indicatorColor = "bg-red-500"
-          }
-
-          const isCurrentHour = entry.startDate === currentHour
-          return (
-            <>
-              <div
-                key={index}
-                className={`flex items-center justify-between my-2 p-3 rounded-lg transition-all duration-300 ${isCurrentHour ? "bg-blue-600 text-white border border-blue-400 shadow-lg" : ""}`}
-              >
-                <div className={`w-3 h-3  ${indicatorColor} mr-2`} />
-                <span className="flex-grow text-left">{formatStartDate(entry.startDate)}</span>
-                <span className="text-right">{entry.price} c/kWh</span>
-              </div>
-              <Separator className="my-2" />
-            </>
-          )
-        })}
-      </div>
-    </ScrollArea>
-  )
 }
 
 // Main App Component
