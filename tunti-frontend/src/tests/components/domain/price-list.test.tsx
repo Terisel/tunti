7603d087af8c35738f5d541ffb2b486f85c900c1
Vainioleo <leo.vainio@solita.fi>
2024-10-10 15:31:19 +0300

    korjaa price-list.test samanlaiseksi kuin app.test.tsx

--- tunti-frontend/src/tests/components/domain/price-list.test.tsx
+++ tunti-frontend/src/tests/components/domain/price-list.test.tsx
@@ -1,39 +1,39 @@
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import { PriceList } from "@/components/domain/price-list"
-import { PriceProvider, usePriceContext} from "@/contexts/price-context.tsx"
-
+import { PriceProvider, usePriceContext } from "@/contexts/price-context.tsx"
 
 // Mocking the usePriceContext hook
 jest.mock("@/contexts/price-context.tsx", () => ({
-    PriceProvider: ({ children }) => <div>{children}</div>,
-    usePriceContext: jest.fn(),
-  }));
+  PriceProvider: ({ children }) => <div>{children}</div>,
+  usePriceContext: jest.fn()
+}))
 
-  const mockPriceData = [
-    { price: 5, startDate: '2024-10-09T09:00:00.000Z', endDate: '2024-10-09T10:00:00.000Z' },
-    { price: 10, startDate: '2024-10-09T10:00:00.000Z', endDate: '2024-10-09T11:00:00.000Z' },
-  ];
+const mockPriceData = [
+  { price: 5, startDate: "2024-10-09T09:00:00.000Z", endDate: "2024-10-09T10:00:00.000Z" },
+  { price: 10, startDate: "2024-10-09T10:00:00.000Z", endDate: "2024-10-09T11:00:00.000Z" }
+]
 
-  describe('PriceList Component', () => {
-    beforeEach(() => {
-      // Set up the mocked return value for usePriceContext
-      (usePriceContext as jest.Mock).mockReturnValue({
-        priceData: mockPriceData,
-        loading: false,
-        error: '',
-      });
-    });
+describe("PriceList Component", () => {
+  beforeEach(() => {
+    // Set up the mocked return value for usePriceContext
+    ;(usePriceContext as jest.Mock).mockReset()
+  })
 
-    test('renders price list with price data', () => {
-      render(
-        <PriceProvider>
-          <PriceList />
-        </PriceProvider>
-      );
+  test("renders price list with price data", () => {
+    ;(usePriceContext as jest.Mock).mockReturnValue({
+      priceData: mockPriceData,
+      loading: false,
+      error: ""
+    })
+    render(
+      <PriceProvider>
+        <PriceList />
+      </PriceProvider>
+    )
 
-      // Assert that the correct price data is displayed
-      expect(screen.getByText(/5 c\/kWh/i)).toBeInTheDocument();
-      expect(screen.getByText(/10 c\/kWh/i)).toBeInTheDocument();
-    });
-  });
+    // Assert that the correct price data is displayed
+    expect(screen.getByText(/5 c\/kWh/i)).toBeInTheDocument()
+    expect(screen.getByText(/10 c\/kWh/i)).toBeInTheDocument()
+  })
+})
