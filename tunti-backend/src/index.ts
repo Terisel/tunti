beacc81494dc695e5057ad4cfab80237716006b1
Vainioleo <leo.vainio@solita.fi>
2024-10-14 15:17:51 +0300

    korjaa pois käyttämättömät reitit ja aja pritterien läpi

--- tunti-backend/src/index.ts
+++ tunti-backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import cors from 'cors';
-import https from 'https';
+import cors from "cors";
+import https from "https";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'https://terisel.github.io', 
-  methods: 'GET', 
-  optionsSuccessStatus: 200 
+  origin: "https://terisel.github.io",
+  methods: "GET",
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions)); // Use CORS with the specified options
@@ -18,42 +18,34 @@ app.use(cors(corsOptions)); // Use CORS with the specified options
 app.get("/api/latest-prices", (req, res) => {
   const url = "https://api.porssisahko.net/v1/latest-prices.json";
 
-  https.get(url, (response) => {
-    let data = '';
+  https
+    .get(url, (response) => {
+      let data = "";
 
-    // A chunk of data has been received.
-    response.on('data', (chunk) => {
-      data += chunk;
+      // A chunk of data has been received.
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received.
+      response.on("end", () => {
+        try {
+          const jsonData = JSON.parse(data); // Parse the JSON data
+          res.status(200).json(jsonData); // Send the fetched data to the frontend
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+          res.status(500).send({ message: "Internal Server Error" });
+        }
+      });
+    })
+    .on("error", (error) => {
+      console.error("Error fetching latest prices:", error);
+      res.status(500).send({ message: "Internal Server Error" });
     });
-
-    // The whole response has been received.
-    response.on('end', () => {
-      try {
-        const jsonData = JSON.parse(data); // Parse the JSON data
-        res.status(200).json(jsonData); // Send the fetched data to the frontend
-      } catch (error) {
-        console.error("Error parsing JSON:", error);
-        res.status(500).send({ message: "Internal Server Error" });
-      }
-    });
-  }).on('error', (error) => {
-    console.error("Error fetching latest prices:", error);
-    res.status(500).send({ message: "Internal Server Error" });
-  });
-});
-
-// New route to return latest prices
-app.get("/new-api/v1/hinnat", async (req, res) => {
-  try {
-    // Return the latest prices data
-    res.status(200).json(latestPrices);
-  } catch (error) {
-    console.error("Error fetching latest prices:", error);
-  }
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello, TypeScript with Express!");
+  res.send("Hello");
 });
 
 app.listen(PORT, () => {
